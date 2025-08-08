b7269baa3becd73c29b38d5ad23081a5f7caf079
mxthing <mxthing@gmail.com>
2025-08-08 14:41:36 +0800

    Update pkg_adapter.rb

--- config/initializers/pkg_adapter.rb
+++ config/initializers/pkg_adapter.rb
@@ -1,6 +1,7 @@
 PkgAdapter.setup do |config|
   config.adapters = {
                     "ios" => {class_name:"Ipa",ext_name:"ipa",des:"iOS"},
-                    "android" => {class_name:"Apk",ext_name:"apk",des:"Android"}
+                    "android" => {class_name:"Apk",ext_name:"apk",des:"Android"},
+                    "aab" => {class_name:"Aab",ext_name:"aab",des:"Android App Bundle"}
                     }
 end
